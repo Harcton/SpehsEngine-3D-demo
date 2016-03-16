1e00854abd4675f262170fa26c0390b524f095d1
Juuso Turunen <juusoturunen94@hotmail.com>
2016-03-16 14:16:36 +0200

    Batch drawing testing WIP

--- Demo3D/Main.cpp
+++ Demo3D/Main.cpp
@@ -4,47 +4,68 @@
 #include <SpehsEngine/InputManager.h>
 #include <SpehsEngine/Window.h>
 #include <SpehsEngine/ApplicationData.h>
-#include <SpehsEngine/PolygonBatch.h>
-#include <SpehsEngine/Camera3D.h>
-#include "Camera3DController.h"
+
+#include <SpehsEngine/Camera2D.h>
+#include <SpehsEngine/BatchManager.h>
+#include <SpehsEngine/Polygon.h>
+
+#include <vector>
+
+
+void update();
+void render();
+void input();
+
+static bool run = true;
+spehs::BatchManager* batchManager = new spehs::BatchManager(&camera);
 
 void main()
 {
 
-	spehs::initialize("3D Demo");
+	std::vector<spehs::Polygon*> polygons;
+
+	polygons.push_back(batchManager->createPolygon());
+
+	spehs::initialize("spehs_engine_testing_zone");
 	mainWindow->clearColor(0, 0, 0, 1.0f);
 	spehs::console::addVariable("fps", applicationData->showFps);
 	spehs::console::addVariable("maxfps", applicationData->maxFps);
-
-	//Camera controller
-	spehs::Camera3D camera;
-	Camera3DController cameraController(camera);
-
-	//Drawables
-	spehs::PolygonBatch square(4, 0.5f, 0.5f);
-
-	bool run = true;
+	
 	while (run)
 	{
-		mainWindow->clearBuffer();
 		spehs::beginFPS();
 
-
-		//Update
-		spehs::console::update();
-		inputManager->update();
-		cameraController.update();
-		if (inputManager->isKeyDown(KEYBOARD_ESCAPE))
-			run = false;
-
-		//Render
-		square.draw();
-		spehs::console::render();
+		update();
+		render();
 
 		spehs::endFPS();
-		spehs::drawFPS();
-		mainWindow->swapBuffers();
 	}
 
+	polygons.clear();
+
 	spehs::uninitialize();
+}
+
+void update()
+{
+	spehs::console::update();
+	inputManager->update();
+	input();
+}
+
+void render()
+{
+	mainWindow->clearBuffer();
+
+	spehs::console::render();
+	spehs::drawFPS();
+	batchManager->render();
+
+	mainWindow->swapBuffers();
+}
+
+void input()
+{
+	if (inputManager->isKeyDown(KEYBOARD_ESCAPE))
+		run = false;
 }
